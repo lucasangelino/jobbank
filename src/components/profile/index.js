96cf2b8799c34273f495cd434665679975e51bb8
Lucas Angelino <angelino.n.lucas@gmail.com>
2021-11-11 11:45:38 -0300

    JBB fetch profile data

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -3,19 +3,25 @@ import * as React from "react";
 import { Text } from "@chakra-ui/layout";
 import Layout from "../layout/Layout";
 import Navbar from "../navbar";
+import { useUser } from "../../hooks/useUser";
 
 export default function Profile() {
   const [profile, setProfile] = React.useState(null);
+  const { is_empresa, id } = useUser();
+
   React.useEffect(() => {
     function getPrefil() {
       const token = localStorage.getItem("token");
-      const res = fetch("http://localhost:5000/api/profile", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const res = fetch(
+        `http://localhost:8080/${is_empresa ? "empresa" : "postulante"}/${id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       const data = res.json();
       setProfile(data);
     }
